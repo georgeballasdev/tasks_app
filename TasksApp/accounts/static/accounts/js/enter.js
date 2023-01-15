2d28e55e6a73dc20412aa809274269a5b91cc28b
georgeballasdev <georgeballas.dev@gmail.com>
2023-01-15 14:17:47 +0200

    Add enter page scroll animation, improve frontend

--- TasksApp/accounts/static/accounts/js/enter.js
+++ TasksApp/accounts/static/accounts/js/enter.js
@@ -1,1 +1,14 @@
-console.log('ok');
+const separator = $('#separator');
+const body = $('html, body');
+let state = 'login';
+
+$(separator).click( () => {
+    if (state == 'login') {
+        body.animate({scrollLeft: 0}, 800);
+        state = 'register';
+    }
+    else {
+        body.animate({scrollLeft: body.width()}, 800);
+        state = 'login';
+    }
+})
